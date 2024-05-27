2124dc2c28f7a4d5a1810cbdbff766b45cffe2cf
Sermartinezm23 <ser.martinezm@duocuc.cl>
2024-05-27 15:52:45 -0400

    Termino semana 2

--- SkeletonAPP/src/app/home/home.page.ts
+++ SkeletonAPP/src/app/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MyServicioService } from '../myservicio.service';
 import { DatePipe } from '@angular/common';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -14,7 +15,12 @@ export class HomePage implements OnInit {
   showDatePicker: boolean = false;
   selectedDate: string = '';
 
-  constructor(private myServicio: MyServicioService, private fb: FormBuilder, private datePipe: DatePipe) {
+  constructor(
+    private myServicio: MyServicioService,
+    private fb: FormBuilder,
+    private datePipe: DatePipe,
+    private alertController: AlertController
+  ) {
     this.form = this.fb.group({
       nombre: [''],
       apellido: [''],
@@ -46,4 +52,20 @@ export class HomePage implements OnInit {
   limpiarCampos() {
     this.form.reset();
   }
+
+  async mostrarDatos() {
+    const nombre = this.form.get('nombre')?.value || '';
+    const apellido = this.form.get('apellido')?.value || '';
+    const nivelEducacion = this.form.get('nivelEducacion')?.value || '';
+    const fechaNacimiento = this.form.get('fechaNacimiento')?.value || '';
+
+    const alert = await this.alertController.create({
+      message: `Su nombre es: ${nombre} ${apellido}`,
+      buttons: ['SI']
+    });
+
+    await alert.present();
+  }
 }
+
+
