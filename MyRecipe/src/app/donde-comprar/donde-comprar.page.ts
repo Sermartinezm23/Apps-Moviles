f3c2cf834f79692099e9d654fb5389cdb5c85acf
Sermartinezm23 <ser.martinezm@duocuc.cl>
2024-06-23 22:26:21 -0400

    Agrego cambios finales prueba 2

--- MyRecipe/src/app/donde-comprar/donde-comprar.page.ts
+++ MyRecipe/src/app/donde-comprar/donde-comprar.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Geolocation } from '@capacitor/geolocation';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-donde-comprar',
@@ -6,10 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./donde-comprar.page.scss'],
 })
 export class DondeComprarPage implements OnInit {
+  map: L.Map | null = null;
+  latitude: number = 0;
+  longitude: number = 0;
 
   constructor() { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    try {
+      const coordinates = await Geolocation.getCurrentPosition();
+      this.latitude = coordinates.coords.latitude;
+      this.longitude = coordinates.coords.longitude;
+
+      this.loadMap();
+    } catch (error) {
+      console.error('Error getting location', error);
+    }
   }
 
+  loadMap() {
+    if (this.latitude !== 0 && this.longitude !== 0) {
+      this.map = L.map('mapId').setView([this.latitude, this.longitude], 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '© OpenStreetMap contributors'
+      }).addTo(this.map);
+
+      L.marker([this.latitude, this.longitude]).addTo(this.map)
+        .bindPopup('Estás aquí')
+        .openPopup();
+    }
+  }
 }
